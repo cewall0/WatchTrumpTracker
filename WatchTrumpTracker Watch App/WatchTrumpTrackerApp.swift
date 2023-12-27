ef223aba9b2a1bf4bf5ee06e667cc01d02462108
Chad Wallace <cewall0@gmail.com>
2023-12-27 14:36:08 -0500

    Initial Commit

---
+++ WatchTrumpTracker Watch App/WatchTrumpTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTrumpTrackerApp.swift
+//  WatchTrumpTracker Watch App
+//
+//  Created by Chad Wallace on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchTrumpTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
